44078aca725adeb0e9a329d73b3ba57edee040f6
Jujej <jpdurau@gmail.com>
2025-05-14 10:01:47 -0300

    Arrumando Jogo.cs e Feedback.cs

--- Models/Feedback.cs
+++ Models/Feedback.cs
@@ -1,4 +1,18 @@
 namespace PlataformaFbj.Models 
 {
-    
+    // Models/Feedback.cs
+public class Feedback
+{
+    public int Id { get; set; }
+    public string Comentario { get; set; }
+    public int Avaliacao { get; set; } // Ex: 1 a 5 estrelas
+    public DateTime DataPublicacao { get; set; }
+
+    // Relação: Um feedback pertence a um usuário e a um jogo
+    public int UsuarioId { get; set; }
+    public Usuario Usuario { get; set; }
+
+    public int JogoId { get; set; }
+    public Jogo Jogo { get; set; }
+}
 }
--- Models/Jogo.cs
+++ Models/Jogo.cs
@@ -1,4 +1,14 @@
 namespace PlataformaFbj.Models
 {
+public class Jogo
+{
+    public int Id { get; set; }
+    public string Titulo { get; set; }
+    public string Genero { get; set; }
+    public string Plataforma { get; set; } // Ex: PC, PlayStation, Xbox
+    public DateTime DataLancamento { get; set; }
 
+    // Relação: Um jogo pode ter vários feedbacks
+    public ICollection<Feedback> Feedbacks { get; set; }
+}
 }
